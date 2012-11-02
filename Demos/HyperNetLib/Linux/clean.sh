398ee37125499213dab0467fa0108b1c8f6295a4
William <william201210@163.com>
2012-11-02 16:33:51 +0800

    Initial Version

---
+++ Demos/HyperNetLib/Linux/clean.sh
@@ -0,0 +1,6 @@
+rm -rf 	HyperNetLibTest
+rm -rf 	../Windows/Debug/
+rm -rf 	../Windows/HyperNetLibTest.suo
+rm -rf 	../Windows/HyperNetLibTest/Debug/
+rm -rf 	../Windows/HyperNetLibTest/HyperNetLibTest.vcxproj.user
+
